166bdad02e697a7fc9a8277fe057d0c59aadaab1
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-06-25 17:40:48 +0100

    added EXAMPLE handlebar files.

--- controllers/API/index.js
+++ controllers/API/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const blogRoutes = require('./blog-routes');
 const commentRoutes = require('./comment-routes');
+const homeRoutes = require('../home-routes');
 
 router.use('/user', userRoutes);
 router.use('/blog', blogRoutes);
 router.use('/comment', commentRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
