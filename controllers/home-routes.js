ad028cec8717bad2b4f3ae29a5638e50f8778459
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-07-03 10:55:21 +0100

    Home page now displays Blogs and comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,18 +2,24 @@ const router = require('express').Router();
 const { User, Blog, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
+
 // Prevent non logged in users from viewing the homepage
 router.get('/',  async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+    // const userData = await User.findAll({
+    //   attributes: { exclude: ['password'] },
+    //   order: [['name', 'ASC']],
+    // });
+
+    //const users = userData.map((project) => project.get({ plain: true }));
+    const blogData = await Blog.findAll({
+      include: [{ model: Comment, as: 'comments' }, { model: User, as: 'user', attributes:['name'] }],
+      order: [['created_date', 'DESC']],
     });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+    console.log(blogs);
     res.render('homepage', {
-      users,
+      blogs,
       // Pass the logged in flag to the template
       loggedIn: req.session.loggedIn,
     });
